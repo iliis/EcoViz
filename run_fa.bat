5cbe0788b70b03e32ea7f788e85381a53e4ef335
Samuel Bryner <SamuelBryner@gmx.ch>
2018-01-20 13:31:54 +0100

    Add bat files to easily launch FAF.

---
+++ run_fa.bat
@@ -0,0 +1,15 @@
+::cd C:\ProgramData\FAForever\bin
+:: "C:\ProgramData\FAForever\bin\ForgedAlliance.exe" /init init_faf.lua /windowed 1024 768
+
+::"C:\ProgramData\FAForever\bin\ForgedAlliance.exe" /init init_faf.lua /windowed 1024 768 /map SCMP_015
+
+:: /map SCMP_015 /faction 1 /nofog /victory "sandbox"
+:: /map "Wonder open 5v5" /victory sandbox /nofog
+::"C:\ProgramData\FAForever\bin\ForgedAlliance.exe" /init init_faf.lua /nobugreport /log "C:\Users\samuel\supcom\logfile.txt" /EnableDiskWatch /showlog /windowed 1024 768 /map SCMP_015 /faction 1 /nofog /victory "sandbox"
+
+::cd "C:\Supreme Commander - Forged Alliance"
+::"C:\Supreme Commander - Forged Alliance\bin\ForgedAlliance.exe" /nobugreport /log "C:\Users\samuel\supcom\logfile.txt" /EnableDiskWatch /showlog /windowed 1024 768 /map SCMP_015 /faction 1 /nofog /victory "sandbox"
+::"C:\ProgramData\FAForever\bin\ForgedAlliance.exe" /init init_faf.lua /nobugreport /log "C:\Users\samuel\supcom\logfile.txt" /EnableDiskWatch /showlog /windowed 1024 768 /map SCMP_015 /faction 1 /nofog /victory "sandbox"
+  
+  
+C:\ProgramData\FAForever\bin\ForgedAlliance.exe /init init_faf.lua /nobugreport /log "C:\Users\samuel\supcom\logfile.txt" /EnableDiskWatch /showlog /windowed 1024 768 /map SCMP_015 /faction 1 /nofog /victory "sandbox"
---
+++ run_fa_to_mainmenu.bat
@@ -0,0 +1,1 @@
+C:\ProgramData\FAForever\bin\ForgedAlliance.exe /init init_faf.lua /nobugreport /log "C:\Users\samuel\supcom\logfile.txt" /EnableDiskWatch /showlog /windowed 1024 768
